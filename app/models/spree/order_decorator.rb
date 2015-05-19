9e3700f945f3e457100fa51a042b280cb77a83c3
David Laprade <david.laprade@gmail.com>
2015-05-19 10:19:40 -0400

    monkey-patch shipping#rate_hash to apply discounts

---
+++ app/models/spree/order_decorator.rb
@@ -0,0 +1,17 @@
+Spree::Order.class_eval do
+  
+  # UPGRADE_CHECK
+  # https://github.com/spree/spree/blob/7d19c8933042cec667634c8cffedcbe6084abf1d/core/app/models/spree/order.rb#L423-L432
+  # apply the the Spree::ShippingMethod's discount to the estimated shipping cost
+  def rate_hash
+    @rate_hash ||= available_shipping_methods.collect do |ship_method|
+      next unless cost = ship_method.calculator.compute(self)
+      Spree::ShippingRate.new( :id => ship_method.id,
+                        :shipping_method => ship_method,
+                        :name => ship_method.name,
+                        :cost => cost * (100 - (ship_method.discount || 0))/100.0,
+                        :currency => currency)
+    end.compact.sort_by { |r| r.cost }
+  end
+
+end
