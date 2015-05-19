63e5c14aecaabf3a7332ae3e9bc9aa0001bebd7f
David Laprade <david.laprade@gmail.com>
2015-05-19 09:45:14 -0400

    customize engine.rb

--- lib/spree_local_tax/engine.rb
+++ lib/spree_local_tax/engine.rb
@@ -1,16 +1,12 @@
-module SpreeLocalTax
+module SpreeDiscountShipping
   class Engine < Rails::Engine
-    engine_name 'spree_local_tax'
-
+    engine_name 'spree_discount_shipping'
+    
     config.autoload_paths += %W(#{config.root}/lib)
 
     # use rspec for tests
     config.generators do |g|
       g.test_framework :rspec
-    end
-
-    initializer 'spree.register.local_tax', :after => "spree.register.calculators" do |app|
-       app.config.spree.calculators.tax_rates << Spree::Calculator::LocalTax
     end
 
     def self.activate
