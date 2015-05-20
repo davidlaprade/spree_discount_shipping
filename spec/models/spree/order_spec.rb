516503e2ad9db19be80cb83769c3c317d997c9c7
David Laprade <david.laprade@gmail.com>
2015-05-20 15:40:04 -0400

    create shipping methods and stub out, randomize model test

--- spec/models/spree/order_spec.rb
+++ spec/models/spree/order_spec.rb
@@ -12,14 +12,15 @@ describe Spree::Order, :type => :model do
     calc.stub(:compute) { |order| 10.0 }
     Spree::ShippingMethod.any_instance.stub(:calculator).and_return(calc)
 
-    # get shipping rates
-    rates = order.rate_hash
-    ship_method = rates.first.shipping_method
+    # create and stub out shipping methods to avoid having to set Spree::Zones, display_on, etc
+    5.times { create :shipping_method, discount: rand(100) }
+    Spree::ShippingMethod.any_instance.stub(:available_to_order?).and_return(true)
 
-    # set discount and calculate cost
-    expect(ship_method.update_attributes( discount: 45 )).to be true
-    calculated_cost = ship_method.calculator.compute(order) * (100 - ship_method.discount)/100.0
-    expect( calculated_cost ).to eq( rates.first.cost )
+    order.rate_hash.each do |rate|
+      ship_method = rate.shipping_method
+      calculated_cost = ship_method.calculator.compute(order) * (100 - ship_method.discount)/100.0
+      expect( calculated_cost ).to eq( rate.cost )
+    end
   end
 
 end
