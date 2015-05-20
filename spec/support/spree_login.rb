70af210f98a6b883bc36ba66c3000fa28ad81926
David Laprade <david.laprade@gmail.com>
2015-05-20 15:35:38 -0400

    put login helper in seperate module
    otherwise it isn't loaded

--- spec/support/spree_login.rb
+++ spec/support/spree_login.rb
@@ -1,7 +1,13 @@
-def login_spree(user)
-  visit spree.login_path
-  fill_in 'Email', :with => user.email
-  fill_in 'Password', :with => 'secret'
-  click_button 'Login'
-  expect(page).to have_content("Logged in successfully")
+module DiscountFeatures
+  def login_spree(user)
+    visit spree.login_path
+    fill_in 'Email', :with => user.email
+    fill_in 'Password', :with => 'secret'
+    click_button 'Login'
+    expect(page).to have_content("Logged in successfully")
+  end
+end
+
+RSpec.configure do |config|
+  config.include DiscountFeatures
 end
