5b9b6da6fc406162e128492e9678df0d72e5864d
Michael Bianco <iloveitaly@gmail.com>
2015-05-27 11:05:02 -0400

    Modifying the shipping method compute_amount method to fix shipping adjustment calculation

--- app/models/spree/shipping_method_decorator.rb
+++ app/models/spree/shipping_method_decorator.rb
@@ -1,3 +1,7 @@
 Spree::ShippingMethod.class_eval do
   attr_accessible :discount
+
+  def compute_amount(calculable)
+    self.calculator.compute(calculable) * (100 - (self.discount || 0))/100.0
+  end
 end
