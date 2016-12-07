599a9c0280bd195df1f48eb855c1a58fa0a0801c
David Laprade <david.laprade@gmail.com>
2016-12-07 15:47:02 -0500

    Rescue and log unknown bug in ShippingMethod#compute_amount

--- app/models/spree/shipping_method_decorator.rb
+++ app/models/spree/shipping_method_decorator.rb
@@ -1,9 +1,16 @@
 Spree::ShippingMethod.class_eval do
   attr_accessible :discount
 
+  DEFAULT_AMOUNT = 10.0
+
   def compute_amount(calculable)
     original = self.calculator.compute(calculable)
     discount = (100 - (self.discount || 0))/100.0 rescue 1
     original * discount
+  rescue
+    Rails.logger.error "Spree::ShippingMethod compute error for " +
+      self.inspect + ", with calculable: " +
+      calculable.inspect + ". Using default amount #{DEFAULT_AMOUNT}"
+    DEFAULT_AMOUNT
   end
 end
